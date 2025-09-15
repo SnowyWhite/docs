78996ab001a9f191ac4c0e060377d46976c81bad
William Roy <wroy@proton.me>
2025-09-15 14:26:18 -0400

    Update URL reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'IW4x Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/iw4x/docs' }],
 			sidebar: [
 				{
 					label: 'Guides',
