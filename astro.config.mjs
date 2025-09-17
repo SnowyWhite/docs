0e88047555000ceba7315cfc8760e06f88acbbbc
Snowy White <45012141+SnowyWhite@users.noreply.github.com>
2025-09-18 01:10:55 +0200

    chore: Customize styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from "astro/config";
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'static',
+	site: 'https://docs.iw4x.io',
+	image: {
+    service: sharpImageService({ limitInputPixels: false }),
+  },
 	integrations: [
 		starlight({
 			title: 'IW4x Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/iw4x/docs' }],
-			sidebar: [
-				{ label: 'Welcome 👋', slug: '' },
-				{ label: 'Changelog', slug: 'guides/changelog' },
+			favicon: '/favicon.svg',
+			customCss: [ './src/styles/custom.css' ],
+			social: [
 				{
-					label: 'Installing IW4x',
-					items: [
-						{ label: 'Installing on Windows', slug: 'guides/install-windows' },
-						{ label: 'Installing on Linux', slug: 'guides/install-linux' },
-					],
-				}
+					icon: 'github', 
+					label: 'GitHub',
+					href: 'https://github.com/iw4x/docs' 
+				},
+				{
+					icon: 'discord',
+					label: 'Discord',
+					href: 'https://discord.com/invite/pV2qJscTXf',
+				},
+				{
+					icon: 'twitter',
+					label: 'Twitter',
+					href: 'https://x.com/IW4x_dev',
+				},
+			],
+			sidebar: [
+				{ label: 'Getting Started', autogenerate: { directory: 'install' } }
 			],
 		}),
 	],
+
+	vite: {
+		server: {
+			allowedHosts: [ 'docs.iw4x.io' ]
+		}
+	}
 });
