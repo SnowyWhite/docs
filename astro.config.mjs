dda739b957168e793f3136fdd282dde25e39277f
Snowy White <45012141+SnowyWhite@users.noreply.github.com>
2025-09-23 01:28:13 +0200

    Add quickstart guide with launcher and manual install guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,50 +7,63 @@ import starlightLinksValidator from 'starlight-links-validator'
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	site: 'https://docs.iw4x.io',
-	image: {
-		service: sharpImageService({ limitInputPixels: false }),
-	},
-	integrations: [
-		starlight({
-			title: 'IW4x Docs',
-			favicon: '/favicon.svg',
-			customCss: [ './src/styles/custom.css' ],
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/iw4x'
-				},
-				{
-					icon: 'discord',
-					label: 'Discord',
-					href: 'https://discord.com/invite/pV2qJscTXf',
-				},
-				{
-					icon: 'twitter',
-					label: 'Twitter',
-					href: 'https://x.com/IW4x_dev',
-				},
-			],
-			sidebar: [
-				{ label: 'Getting Started', autogenerate: { directory: 'install' } }
-			],
-			editLink: {
-				baseUrl: 'https://github.com/iw4x/docs/edit/main/'
-			},
-			plugins: [
-				starlightAutoDrafts(),
-				starlightImageZoom(),
-				starlightLinksValidator()
-			],
-		}),
-	],
+  output: 'static',
+  site: 'https://docs.iw4x.io',
+  image: {
+    service: sharpImageService({ limitInputPixels: false }),
+  },
+  integrations: [
+    starlight({
+      title: 'IW4x Docs',
+      favicon: '/favicon.svg',
+      customCss: ['./src/styles/custom.css'],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            { label: 'Quickstart', link: '/get-started/quickstart/' },
+            {
+              label: 'Manual Installation',
+              items: [
+                { label: 'On Windows', link: '/get-started/manual-install/windows-guide/' },
+                { label: 'On Linux', link: '/get-started/manual-install/linux-guide/' },
+                { label: 'On MacOS', link: '/get-started/manual-install/macos-guide/' },
+              ]
+            }
+          ]
+        }
+      ],
+      editLink: {
+        baseUrl: 'https://github.com/iw4x/docs/edit/main/'
+      },
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/iw4x'
+        },
+        {
+          icon: 'discord',
+          label: 'Discord',
+          href: 'https://discord.com/invite/pV2qJscTXf',
+        },
+        {
+          icon: 'twitter',
+          label: 'Twitter',
+          href: 'https://x.com/IW4x_dev',
+        },
+      ],
+      plugins: [
+        starlightAutoDrafts(),
+        starlightImageZoom(),
+        starlightLinksValidator()
+      ],
+    }),
+  ],
 
-	vite: {
-		server: {
-			allowedHosts: [ 'docs.iw4x.io' ]
-		}
-	}
+  vite: {
+    server: {
+      allowedHosts: ['docs.iw4x.io']
+    }
+  }
 });
