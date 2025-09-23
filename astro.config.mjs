109f37e750088ed66e4ae4c88928fe58e0d2483a
William Roy <wroy@proton.me>
2025-09-23 12:05:35 -0400

    Add internationalization (i18n)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,41 +6,82 @@ import starlightImageZoom from 'starlight-image-zoom'
 import starlightLinksValidator from 'starlight-links-validator'
 
 // https://astro.build/config
+//
 export default defineConfig({
   output: 'static',
   site: 'https://docs.iw4x.io',
+
   image: {
     service: sharpImageService({ limitInputPixels: false }),
   },
+
   integrations: [
     starlight({
       title: 'IW4x Docs',
       favicon: '/favicon.svg',
+      defaultLocale: 'root',
+
+      // Internationalization
+      //
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
+        },
+        ru: {
+          label: 'Русский',
+          lang: 'ru',
+        },
+      },
+
+      // Styling
+      //
       customCss: ['./src/styles/custom.css'],
+
+      // Navigation
+      //
       sidebar: [
         {
           label: 'Getting Started',
           items: [
-            { label: 'Quickstart', link: '/get-started/quickstart/' },
+            {
+              label: 'Quickstart',
+              link: '/get-started/quickstart/'
+            },
             {
               label: 'Manual Installation',
               items: [
-                { label: 'On Windows', link: '/get-started/manual-install/windows-guide/' },
-                { label: 'On Linux', link: '/get-started/manual-install/linux-guide/' },
-                { label: 'On MacOS', link: '/get-started/manual-install/macos-guide/' },
-              ]
-            }
-          ]
-        }
+                {
+                  label: 'On Windows',
+                  link: '/get-started/manual-install/windows-guide/'
+                },
+                {
+                  label: 'On Linux',
+                  link: '/get-started/manual-install/linux-guide/'
+                },
+                {
+                  label: 'On MacOS',
+                  link: '/get-started/manual-install/macos-guide/'
+                },
+              ],
+            },
+          ],
+        },
       ],
+
+      // Git integration
+      //
       editLink: {
-        baseUrl: 'https://github.com/iw4x/docs/edit/main/'
+        baseUrl: 'https://github.com/iw4x/docs/edit/main/',
       },
+
+      // Social links
+      //
       social: [
         {
           icon: 'github',
           label: 'GitHub',
-          href: 'https://github.com/iw4x'
+          href: 'https://github.com/iw4x',
         },
         {
           icon: 'discord',
@@ -53,17 +94,22 @@ export default defineConfig({
           href: 'https://x.com/IW4x_dev',
         },
       ],
+
+      // Plugins
+      //
       plugins: [
         starlightAutoDrafts(),
         starlightImageZoom(),
-        starlightLinksValidator()
+        starlightLinksValidator(),
       ],
     }),
   ],
 
+  // Development server configuration
+  //
   vite: {
     server: {
-      allowedHosts: ['docs.iw4x.io']
-    }
-  }
+      allowedHosts: ['docs.iw4x.io'],
+    },
+  },
 });
